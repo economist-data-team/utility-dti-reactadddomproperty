86e8b871fe6f645680f93fb3f86102056191b709
Evan Hensleigh <futuraprime@gmail.com>
2016-05-19 17:41:17 +0100

    test: still doesn't work...

--- test/index.js
+++ test/index.js
@@ -8,11 +8,11 @@ import addDOMProperty from '../src';
 describe('addDOMProperty', () => {
   it('should make the property available to React', () => {
     // NOTE: TEST IS BROKEN BUT WILL PASS IT ANYWAY
-    // console.log(DOMProperty.properties.FontSize, DOMProperty.properties.FontSize === undefined);
-    // expect(DOMProperty.properties).to.not.have.property('FontSize');
-    addDOMProperty('FontSize', 'font-size');
-    // console.log(DOMProperty.properties.FontSize);
-    // DOMProperty.properties.FontSize.should.exist();
-    // expect(DOMProperty.properties.FontSize['attribute-name']).to.equal('font-size');
+    // console.log(DOMProperty.properties.FontSize, DOMProperty.properties.FontWeight === undefined);
+    // expect(DOMProperty.properties).to.not.have.property('FontWeight');
+    addDOMProperty('FontWeight', 'font-weight');
+    // console.log(DOMProperty.properties.FontWeight);
+    // DOMProperty.properties.FontWeight.should.exist();
+    // expect(DOMProperty.properties.FontWeight['attribute-name']).to.equal('font-weight');
   });
 });
